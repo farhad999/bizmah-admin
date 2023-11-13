2ec1bda62eae2fcab955fd63a75b68d38eb51e00
Farhad  Shikder <frd1952@gmail.com>
2023-11-14 00:50:50 +0600

    Form Select Added

---
+++ app/View/Components/Form/Select.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\View\Components\Form;
+
+use Illuminate\View\Component;
+
+class Select extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+
+    public $label;
+
+    public $id;
+
+    public $name;
+
+    public $options;
+
+    public $class;
+
+    public $groupClass;
+
+    public $placeholder;
+
+    public $value;
+
+    public $required;
+
+
+    public function __construct($name, $options, $id = null, $value = '', $label = '', $placeholder = null, $class = null, $required=null)
+    {
+        $this->name = $name;
+        $this->id = $id;
+        $this->value = $value;
+        $this->placeholder = $placeholder;
+        $this->label = $label;
+        $this->class = $class;
+        if (gettype($options) == 'string') {
+
+            $this->options = $this->convertToAssociativeArray($options);
+
+        }else {
+            $this->options = $options;
+        }
+        $this->required =$required;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+
+        return view('components.form.select');
+    }
+
+    private function convertToAssociativeArray($stringArray)
+    {
+        // Remove the opening and closing brackets
+        $stringArray = substr($stringArray, 1, -1);
+
+// Split the string into key-value pairs
+        $pairs = explode(', ', $stringArray);
+
+// Create the associative array
+        $associativeArray = [];
+
+// Convert key-value pairs into an associative array
+        foreach ($pairs as $pair) {
+            // Split each pair into key and value
+            list($key, $value) = explode('=>', $pair);
+
+            // Remove any leading/trailing spaces
+            $key = trim($key);
+
+            // Assign value to the corresponding key in the associative array
+            $associativeArray[$key] = $value;
+        }
+
+        return $associativeArray;
+    }
+
+}
---
+++ resources/views/components/Form/select.blade.php
@@ -0,0 +1,28 @@
+<div class="form-group mb-3">
+
+    @if (!empty($label))
+        <label for="{{ $id ?? $name}}" class="form-label">{{__($label)}}
+            @if(!empty($required))
+                <span class="text-danger ml-1">*</span>
+            @endif
+        </label>
+    @endif
+
+    @if(!is_array($options) && !is_iterable($options))
+        <pre class="text-danger">Data Error</pre>
+    @else
+
+        <select class="form-control" @class="{{$class}}" id="{{$id ?? $name}}" name="{{$name}}" {{$attributes}}
+        @if(!empty($required))
+            required="required"
+            @endif
+        >
+            <option
+                value="">{{ ($placeholder ? '--'. $placeholder.'--' : !empty($label)) ? '--'. __($label). '--' : '--Select One--'}}</option>
+            @foreach($options as $key=>$option)
+                <option value="{{$key}}"
+                        @if(gettype($value) == 'array' ? in_array($key, $value) : strval($key) == strval($value)) selected="selected" @endif>{{$option}}</option>
+            @endforeach
+        </select>
+    @endif
+</div>
