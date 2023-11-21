3f888fb1ed0315436b440a0671a50cd29e950fb4
Farhad  Shikder <frd1952@gmail.com>
2023-11-21 21:32:46 +0600

    Update textarea.blade.php

--- resources/views/components/Form/textarea.blade.php
+++ resources/views/components/Form/textarea.blade.php
@@ -1,15 +1,19 @@
+@props(['id' => null, 'name', 'label'=> '', 'placeholder' => '', 'required' => false, 'value' => '', 'class' => '', 'rows' => 3])
+
 <div class="form-group mb-3">
-    <label for="{{$id ?:$name}}" @class(["$labelClass font-bold"])>{{__($label)}}
-        @if(!empty($required))
-            <span class="text-danger ml-1">*</span>
-        @endif
-    </label>
-    <textarea id="{{$id ?: $name}}" name="{{$name}}"
-              placeholder="{{!empty($placeholder) ? __($placeholder):  __($label)}}"
-              rows="{{$rows}}"
-              @class(["$class form-control"]) {{$attributes}}
-              @if(!empty($required))
-                  required="required"
+  <label
+    @class(["font-weight-bold"])>{{__($label)}}
+    @if(!empty($required))
+      <span class="text-danger ml-1">*</span>
+    @endif
+  </label>
+  <textarea
+    name="{{$name}}"
+    placeholder="{{!empty($placeholder) ? __($placeholder):  __($label)}}"
+    rows="{{$rows}}"
+    @class(["$class form-control"]) {{$attributes}}
+    @if(!empty($required))
+      required="required"
            @endif
     >{{old($name) ?? ($value != null ? $value : '')}}</textarea>
 </div>
