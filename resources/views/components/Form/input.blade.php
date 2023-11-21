d36712bc51487e7116ed963254483ee9d1525070
Farhad  Shikder <frd1952@gmail.com>
2023-11-21 21:33:41 +0600

    id will not be added if not provided form.input

--- resources/views/components/Form/input.blade.php
+++ resources/views/components/Form/input.blade.php
@@ -3,19 +3,19 @@
   'labelClass' => ''])
 
 <div class="form-group mb-3">
-    @if(!empty($label))
-        <label for="{{ $id ?? $name}}" @class(["$labelClass text-capitalize form-label"])>{{__($label)}}
-            @if(!empty($required))
-                <span class="text-danger ml-1">*</span>
-            @endif
-        </label>
+  @if(!empty($label))
+    <label @class(["$labelClass text-capitalize form-label"])>{{__($label)}}
+      @if(!empty($required))
+        <span class="text-danger ml-1">*</span>
+      @endif
+    </label>
+  @endif
+  <input name="{{$name}}"
+         placeholder="{{$placeholder ? __($placeholder):  __($label)}}"
+         @class(["$class form-control"]) {{$attributes}}
+         value="{{old($name) ?? ($value != null ? $value : '')}}"
+         @if(!empty($required))
+           required="required"
     @endif
-    <input id="{{$id ?? $name}}" name="{{$name}}"
-           placeholder="{{$placeholder ? __($placeholder):  __($label)}}"
-           @class(["$class form-control"]) {{$attributes}}
-           value="{{old($name) ?? ($value != null ? $value : '')}}"
-           @if(!empty($required))
-               required="required"
-        @endif
-    >
+  >
 </div>
