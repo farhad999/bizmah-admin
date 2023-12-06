c65a7a739be838c60efe4912c811082e15aa4ce3
Farhad  Shikder <frd1952@gmail.com>
2023-12-06 21:09:49 +0600

    logo updated

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -8,10 +8,9 @@ $configData = Helper::appClasses();
   @if(!isset($navbarFull))
   <div class="app-brand demo">
     <a href="{{url('/')}}" class="app-brand-link">
-      <span class="app-brand-logo demo">
-        @include('_partials.macros',["height"=>20])
-      </span>
-      <span class="app-brand-text demo menu-text fw-bold">{{config('variables.templateName')}}</span>
+        <img src="{{asset('assets/img/logo.png')}}" alt="logo"
+        class="w-75"
+        >
     </a>
 
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
