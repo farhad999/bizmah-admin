b154d720d5a32ef7aaae24de522635de46518b58
Farhad  Shikder <frd1952@gmail.com>
2023-12-09 20:48:38 +0600

    update user action buttons

--- resources/views/user/action-buttons.blade.php
+++ resources/views/user/action-buttons.blade.php
@@ -1,5 +1,13 @@
-<td>
-  <a href="{{ route('users.edit', $row->id) }}" class="icon">
-    <i class="ti ti-edit"></i>
-  </a>
-</td>
+@if($row->username != 'admin')
+  <td>
+    <a href="{{ route('users.edit', $row->id) }}" class="btn btn-primary btn-sm icon">
+      <i class="ti ti-edit"></i>
+    </a>
+  </td>
+
+  <td>
+    <button data-href="{{ route('users.destroy', $row->id) }}" class="btn btn-danger btn-sm icon delete-item-btn">
+      <i class="ti ti-trash"></i>
+    </button>
+  </td>
+@endif
