93faa108a8b13f5c3b3ce60fe4ad9a2f7672c0e2
Farhad  Shikder <frd1952@gmail.com>
2023-12-10 00:07:08 +0600

    Update index.blade.php

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.layoutMaster')
 
 @section('content')
-  <x-content title="Users">
-    <x-slot name="buttons">
+  <x-content title="Customers">
+    {{--<x-slot name="buttons">
       <a href="{{route("customers.create")}}" class="btn btn-primary">Add Customer</a>
-    </x-slot>
+    </x-slot>--}}
 
     <table class="table table-hover" id="datatable">
       <thead>
